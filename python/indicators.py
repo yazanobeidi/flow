486a0b40fcbbc94751b0630aac2efe68b4b8f498
Matthew Robichaud <maro@Matthews-MacBook-Air.local>
2016-02-28 15:43:17 -0500

    working indicators

--- python/indicators.py
+++ python/indicators.py
@@ -18,10 +18,14 @@ class Indicators(object):
         return self.state
 
     def moving_average(self, size, sliced):
-        multiplier = (2/(size + 1))
-        ema = 0
+        multiplier = 0.0
+        multiplier = float((2/(float(size) + 1)))
+        ema = 0.0
         for value in sliced:
             ema = (multiplier*value) + ((1-multiplier)*ema)
+
+        if (ema == 0 and sum(sliced) != 0):
+            print("WE GOT A EMA PROBLEM MAWFUCKA")
         return ema
 
     def crossover_indicator(self, q, x, y):
@@ -35,9 +39,6 @@ class Indicators(object):
                 return 1
         return 0
 
-    def print_quotes(self):
-        print(crossover_indicator(5, 10))
-
 #https://en.wikipedia.org/wiki/MACD#Mathematical_interpretation
     def MACD(self, q, m1, m2):
         signal = self.moving_average(m1, q[-m1:]) - self.moving_average(m2, q[-m2:])
@@ -47,11 +48,11 @@ class Indicators(object):
         series = []
         i = 0
         for quotes in q:
-            if m2 < i:
-                series[i] = self.moving_average(m1, q[-m1-i:-i]) - self.moving_average(m2, q[-m2-i:-i])
+            if m2 > i:
+                series.append(self.moving_average(m1, q[-m1-i:-i]) - self.moving_average(m2, q[-m2-i:-i]))
             i += 1
         if m2 < i:                                                
-            series[i] = self.MACD(q,m1,m2)                                               
+            series.append(self.MACD(q,m1,m2))                                              
         return series
 
     def MACD_sig_line(self, q, m1, m2, m3):
@@ -61,6 +62,12 @@ class Indicators(object):
                 return -1
         elif self.MACD(q, m1, m2) > self.moving_average(m3, MACD_series[-m2:]):
             if self.MACD(q[:-1], m1, m2) < self.moving_average(m3, MACD_series[-m2-1:-1]):
+        self.series = self.MACD_series(q, m1, m2)
+        if self.MACD(q, m1, m2) < self.moving_average(m3, self.series[-m2:]):
+            if self.MACD(q[:-1], m1, m2) > self.moving_average(m3, self.series[-m2-1:-1]):
+                return -1
+        elif self.MACD(q, m1, m2) > self.moving_average(m3, self.series[-m2:]):
+            if self.MACD(q[:-1], m1, m2) < self.moving_average(m3, self.series[-m2-1:-1]):
                 return 1
         return 0
 
@@ -75,25 +82,31 @@ class Indicators(object):
         i = 0
         upcount = 0
         downcount = 0
+        RS = 50.0
         updays = []
         downdays = []
         while (upcount <= period and downcount <= period) and i < len(q) - 1:
             if q[1+i] < q[i]:
-                updays[upcount] = q[1+i]
+                updays.append(q[1+i])
                 upcount += 1
             elif q[1+i] > q[i]:
-                downdays[downcount] = q[1+i]
+                downdays.append(q[1+i])
                 downcount += 1
             i += 1
         try:
             RS = self.moving_average(period, updays) / self.moving_average(period, downdays)
         except:
             RS = 0
+        #print self.moving_average(period, downdays)
+        if float(self.moving_average(period, downdays)) != 0.0:
+            RS = float(self.moving_average(period, updays)) / float(self.moving_average(period, downdays))
+            #print RS
+        #print len(q)
         RSI = (100-(100/(1+RS)))
-        
+        #print RSI
         if RSI < threshold:
             return 1
-        elif RSI > (1-threshold):
+        elif RSI > (100-threshold):
             return -1
         return 0
     
