09d65d22f39b642f775cdfe340771bbdb70d59f9
Matthew Robichaud <maro@Matthews-MacBook-Air.local>
2016-02-28 10:43:13 -0500

    complete order class

--- python/order.py
+++ python/order.py
@@ -1,15 +1,29 @@
-from learning import Learning
 
-#instrument: eg. USDCAD, action: buy/sell
-class Order(instrument, action, quote, volume):
+#action: buy/sell
+class Order():
     def __init__(self):
         self.log = log
-        self.logger.info('Initializing Order...')
+        self.open_cost = []
+        self.close_profit = []
+        self.profit = []
+        
+    def open_order(self, action, quote, volume):
+        self.action = action
+        self.volume = volume
+        self.open_cost = quote*volume
+        self.logger.info('{action} opened by {agent} in {scope} learning.'.format(
+                                                      action=action, agent=self, scope=scope))
 
-    def get_open_cost(self):
-        return quote*volume
+    def close_order(self, quote):
+        self.close_profit = quote*volume
+        self.profit = self.close_profit - self.open_cost
+        self.logger.info('{action} closed by {agent} in {scope} learning. Profit = ${profit}.'.format(
+                                                      action=action, agent=self, scope=scope, profit = self.profit))
+        
+    def get_profit(self):
+        return self.profit
 
-    def 
+    
         
     
 
