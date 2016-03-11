98282a30c7e4ee41a31a2d55aa7f5d104980ff46
Matthew Robichaud <maro@Matthews-MacBook-Air.local>
2016-03-10 22:19:08 -0500

    no

--- python/bankroll.py
+++
@@ -1,9 +0,0 @@
-class Bankroll(object):
-    def __init__(self):
-        self.bankroll = 1000
-
-    def add_profit(self, profit):
-        self.bankroll += profit
-
-    def get_bankroll(self):
-        return self.bankroll
